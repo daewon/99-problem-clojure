3f7991edb71e26d0abd5375bc67ad89340124069
daewon <blueiur@mint>
2012-07-29 18:54:32 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject daewon "0.1"
+  :description "daewon's clojure example"
+  :url "http://daewon.github.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-swank "1.4.4"]]
+  :main daewon.core)
---
+++ test/daewon/core_test.clj
@@ -0,0 +1,7 @@
+(ns daewon.core-test
+  (:use clojure.test
+        daewon.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
