c89b2ec40639df4106bad4cd5788545a213e1f43
daewon <blueiur@gmail.com>
2014-05-26 16:49:44 +0900

    -a

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://daewon.github.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[lein-swank "1.4.5"]]
   :main daewon.core)
